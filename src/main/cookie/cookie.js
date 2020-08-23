9f25b152cbf7fd8b47a7cd886b78963ae59d83b0
jiapril11 <tadadadacoding@gmail.com>
2020-08-23 23:56:26 +0900

    chg pd of intro, brand section on tablet, mobile

--- src/main/cookie/cookie.js
+++ src/main/cookie/cookie.js
@@ -44,8 +44,8 @@ function eventBinding_cookie(){
     $clxBox.on('click', function(){
 
         if($('#no_ck').is(':checked')){
-            // setCookie('today', 'done', 1)
-            $.cookie('today', 'done', {expires: 1, path: '/'});
+            setCookie('today', 'done', 1)
+            // $.cookie('today', 'done', {expires: 1, path: '/'});
         }
         $cookieBox.stop().animate({top:-60},500).removeClass('on');
         $header.stop().animate({top : 0}, 500);
